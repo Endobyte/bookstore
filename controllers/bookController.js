c7de07b069643c6486d998508d1f55581549301d
crl2 <crl2@fresnou.org>
2023-02-24 11:37:52 -0800

    Finish functionality for books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,15 +1,73 @@
 const {Book} = require('../models');
+const genres = ['Horror', 'Science Fiction', 'Dystopian Fiction', 'Historical Fiction', 'Non-Fiction', 'Biography', 'Fiction', 'Mystery'].sort();
 
 // view all
+module.exports.viewAll = async function (req, res) {
+    const books = await Book.findAll();
+    res.render('book/view_all', {books});
+}
 
 // profile
+module.exports.viewProfile = async function (req, res) {
+    const book = await Book.findByPk(req.params.id);
+    res.render('book/profile', {book});
+}
 
 // render add form
+module.exports.renderAddForm = function (req, res) {
+    const book = {
+        title: '',
+        publisher: '',
+        genre: genres[0],
+        pages: '',
+        cover: '',
+        description: ''
+    }
+    res.render('book/add', {book, genres});
+}
 
 // add
+module.exports.addBook = async function (req, res) {
+    const book = await Book.create({
+        title: req.body.title,
+        publisher: req.body.publisher,
+        genre: req.body.genre,
+        pages: req.body.pages,
+        cover: req.body.cover,
+        description: req.body.description
+    })
+    res.redirect(`/books/profile/${book.id}`);
+}
 
 // render edit form
+module.exports.renderEditForm = async function (req, res) {
+    const book = await Book.findByPk(req.params.id);
+    res.render('book/edit', {book, genres});
+}
 
 // update
+module.exports.updateBook = async function (req, res) {
+    const book = await Book.update({
+        title: req.body.title,
+        publisher: req.body.publisher,
+        genre: req.body.genre,
+        pages: req.body.pages,
+        cover: req.body.cover,
+        description: req.body.description
+    }, {
+        where: {
+            id: req.params.id
+        }
+    })
+    res.redirect(`/books/profile/${req.params.id}`);
+}
 
 // delete
+module.exports.deleteBook = async function (req, res) {
+    await Book.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.redirect('/books');
+}
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,13 @@ const bookController = require('../controllers/bookController');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/books', bookController.viewAll);
+router.get('/books/profile/:id', bookController.viewProfile);
+router.get('/books/edit/:id', bookController.renderEditForm);
+router.get('/books/add', bookController.renderAddForm);
+router.get('/books/delete/:id', bookController.deleteBook);
+
+router.post('/books/edit/:id', bookController.updateBook);
+router.post('/books/add', bookController.addBook);
 
 module.exports = router;
